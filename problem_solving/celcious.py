0ff37d9efbc5b885cbaaf9e8ff434b66fd7561a3
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2025-02-22 23:23:22 +0600

    Update celcious.py
    calciols convert

--- problem_solving/celcious.py
+++ problem_solving/celcious.py
@@ -1,3 +1,7 @@
 celcious = float(input('Entery You Celcious valus : '))
 fahrenheit = (celcious* (9/5))+32
 print(f"Current temperature {fahrenheit}")
+
+celcious = float(input('Entery You Celcious valus : '))
+fahrenheit = (celcious* (9/5))+32
+print(f"Current temperature {fahrenheit}")
