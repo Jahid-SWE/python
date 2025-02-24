6f84cd0b4fd51d9e31e68774973a47ac82d728e4
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2025-02-24 23:52:31 +0600

    Update even_odd.py
    Even number are establish

--- problem_solving/even_odd.py
+++ problem_solving/even_odd.py
@@ -1,5 +1,6 @@
 number = int(input("Enter your number first : "))
 if number%2 == 0:
     print(f'This is even number {number}')
+    print(f'This is even number {number}')
 else:
     print(f'This is odd number {number}')
